ac4e1a98d87267fe385def74c78c85e686440804
William Findlay <will@isovalent.com>
2022-12-21 00:58:12 -0500

    api: change signature of games function
    Abstract away the Filters struct from the function signature.
    
    Signed-off-by: William Findlay <will@isovalent.com>

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,13 @@ use futures::Stream;
 
 use pagination::{PaginatedRequest, PaginationClient};
 use types::{
-    games::{self, Game, GamesPlayed},
+    games::{self, Game, GamesPlayed, Leaderboard},
     profile::Profile,
 };
+
+// Rexports
+pub use chrono;
+pub use futures;
 
 /// Get profile stats for a player.
 pub async fn profile(profile_id: u64) -> Result<Profile> {
@@ -33,17 +37,26 @@ pub async fn profile(profile_id: u64) -> Result<Profile> {
 }
 
 /// Get games for a player.
+///
+/// # Params
+/// - `profile_id` is aoe4world the ID of the player whose games should be searched.
+/// - `leaderboard` is an optional leaderboard to be searched against (e.g.
+/// [`Leaderboard::RmTeam`]).
+/// - `opponent_ids` is an optional list of opponent profile IDs to search against.
 pub async fn games(
     profile_id: u64,
-    filter: Option<games::Filters>,
+    leaderboard: Option<Leaderboard>,
+    opponent_ids: Option<&[u64]>,
+    since: Option<chrono::DateTime<chrono::Utc>>,
 ) -> Result<impl Stream<Item = Result<Game>>> {
     let client = PaginationClient::<GamesPlayed, Game>::default();
     let url = format!("https://aoe4world.com/api/v0/players/{}/games", profile_id).parse()?;
-    let url = if let Some(filter) = filter {
-        filter.query_params(url)
-    } else {
-        url
+    let filter = games::Filters {
+        leaderboard,
+        opponent_profile_ids: opponent_ids.map(|ids| ids.to_vec()).unwrap_or_default(),
+        since,
     };
+    let url = filter.query_params(url);
     let pages = client
         .into_pages_concurrent(PaginatedRequest::new(url))
         .await?;
@@ -69,7 +82,7 @@ mod tests {
     #[cfg_attr(not(feature = "test-api"), ignore)]
     #[tokio::test(flavor = "multi_thread")]
     async fn games_api_smoke() {
-        let games: Vec<_> = games(ONLY_CAMS_ID.into(), None)
+        let games: Vec<_> = games(ONLY_CAMS_ID.into(), None, None, None)
             .await
             .expect("API call should succeed")
             .collect()
