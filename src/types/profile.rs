1bce419d15567121b341371c1271dfd67545a82d
William Findlay <will@isovalent.com>
2022-12-27 01:57:13 -0500

    tests: test profiles deserialized values
    Signed-off-by: William Findlay <will@isovalent.com>

--- src/types/profile.rs
+++ src/types/profile.rs
@@ -231,11 +231,14 @@ pub struct RatingHistoryEntry {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
-    use crate::testutils::assert_serde_roundtrip;
+    use crate::{testutils::assert_serde_roundtrip, types::rank::RankDivision};
 
     use arbitrary::Arbitrary;
+    use pretty_assertions::assert_eq;
     use serde_json::from_str;
 
     const NEPTUNE_JSON: &str = include_str!("../../testdata/neptune.json");
@@ -244,7 +247,136 @@ mod tests {
     #[test]
     fn profile_examples_deserialize_smoke() {
         let _: Profile = from_str(NEPTUNE_JSON).expect("neptune should deserialize");
-        let _: Profile = from_str(HOUSEDHORSE_JSON).expect("housedhorse should deserialize");
+
+        let profile: Profile = from_str(HOUSEDHORSE_JSON).expect("housedhorse should deserialize");
+        let profile_expected = Profile {
+            name: "[DEBILS] HousedHorse".into(),
+            profile_id: 3176.into(),
+            steam_id: Some("76561198094298920".into()),
+            site_url: Some("http://aoe4world.com/players/3176".try_into().unwrap()),
+            avatars: Some(Avatars {
+                small: Some( "https://avatars.akamai.steamstatic.com/220b1f84a5204c3e79087cfa0a197045ec0fb0f2.jpg".try_into().unwrap()),
+                medium: Some( "https://avatars.akamai.steamstatic.com/220b1f84a5204c3e79087cfa0a197045ec0fb0f2_medium.jpg".try_into().unwrap()),
+                full: Some( "https://avatars.akamai.steamstatic.com/220b1f84a5204c3e79087cfa0a197045ec0fb0f2_full.jpg".try_into().unwrap())
+            }),
+            social: Some(Social {
+                twitch: Some("https://www.twitch.tv/housedhorse".try_into().unwrap()),
+                youtube: None,
+                liquipedia: None,
+                twitter: None,
+                reddit: None,
+                instagram: None
+            }),
+            last_game_at: None,
+            modes: Some(GameModes {
+                rm_solo: None,
+                rm_team: Some(GameModeStats {
+                    rating: Some(1742),
+                    max_rating: Some(1742),
+                    max_rating_7d: Some(1742),
+                    max_rating_1m: Some(1742),
+                    rank: Some(27),
+                    streak: Some(5),
+                    games_count: Some(123),
+                    wins_count: Some(109),
+                    losses_count: Some(14),
+                    drops_count: Some(0),
+                    last_game_at: Some(chrono::DateTime::from_str("2022-12-14T03:10:20.000Z").unwrap()),
+                    win_rate: Some(88.6),
+                    rank_level: Some(RankLeague::Conqueror(RankDivision::Three)),
+                    rating_history: HashMap::from([
+                        ("1669316350".to_owned(), RatingHistoryEntry {
+                            rating: Some(1678),
+                            streak: Some(7),
+                            games_count: Some(113),
+                            wins_count: Some(100),
+                            drops_count: Some(0),
+                        }),
+                        ("1669317710".to_owned(), RatingHistoryEntry {
+                            rating: Some(1687),
+                            streak: Some(8),
+                            games_count: Some(114),
+                            wins_count: Some(101),
+                            drops_count: Some(0),
+                        }),
+                        ("1669325297".to_owned(), RatingHistoryEntry {
+                            rating: Some(1697),
+                            streak: Some(9),
+                            games_count: Some(115),
+                            wins_count: Some(102),
+                            drops_count: Some(0),
+                        }),
+                        ("1669419363".to_owned(), RatingHistoryEntry {
+                            rating: Some(1715),
+                            streak: Some(10),
+                            games_count: Some(116),
+                            wins_count: Some(103),
+                            drops_count: Some(0),
+                        }),
+                        ("1669420489".to_owned(), RatingHistoryEntry {
+                            rating: Some(1731),
+                            streak: Some(11),
+                            games_count: Some(117),
+                            wins_count: Some(104),
+                            drops_count: Some(0),
+                        }),
+                        ("1669471603".to_owned(), RatingHistoryEntry {
+                            rating: Some(1708),
+                            streak: Some(-1),
+                            games_count: Some(118),
+                            wins_count: Some(104),
+                            drops_count: Some(0),
+                        }),
+                        ("1670108125".to_owned(), RatingHistoryEntry {
+                            rating: Some(1709),
+                            streak: Some(1),
+                            games_count: Some(119),
+                            wins_count: Some(105),
+                            drops_count: Some(0),
+                        }),
+                        ("1670110151".to_owned(), RatingHistoryEntry {
+                            rating: Some(1718),
+                            streak: Some(2),
+                            games_count: Some(120),
+                            wins_count: Some(106),
+                            drops_count: Some(0),
+                        }),
+                        ("1670112063".to_owned(), RatingHistoryEntry {
+                            rating: Some(1719),
+                            streak: Some(3),
+                            games_count: Some(121),
+                            wins_count: Some(107),
+                            drops_count: Some(0),
+                        }),
+                        ("1670290359".to_owned(), RatingHistoryEntry {
+                            rating: Some(1729),
+                            streak: Some(4),
+                            games_count: Some(122),
+                            wins_count: Some(108),
+                            drops_count: Some(0),
+                        }),
+                        ("1670987420".to_owned(), RatingHistoryEntry {
+                            rating: Some(1742),
+                            streak: Some(5),
+                            games_count: Some(123),
+                            wins_count: Some(109),
+                            drops_count: Some(0),
+                        }),
+                ]) }),
+                rm_1v1: None,
+                rm_2v2: None,
+                rm_3v3: None,
+                rm_4v4: None,
+                qm_1v1: None,
+                qm_2v2: None,
+                qm_3v3: None,
+                qm_4v4: None,
+                custom: None,
+            }),
+        };
+
+        assert_eq!(profile, profile_expected);
+        assert_serde_roundtrip(profile_expected);
     }
 
     #[test]
