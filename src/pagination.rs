576aff56920b34396184d8deafd993c7c74dcf26
William Findlay <will@isovalent.com>
2023-02-01 19:05:11 -0500

    api: derive clone
    Signed-off-by: William Findlay <will@isovalent.com>

--- src/pagination.rs
+++ src/pagination.rs
@@ -21,7 +21,7 @@ const DEFAULT_COUNT_PER_PAGE: usize = 50;
 ///
 /// This is used as part of the transparent pagination streaming logic.
 /// Should be embedded into paginated data using `#[serde(flatten)]`.
-#[derive(Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone)]
 #[serde(rename_all = "snake_case")]
 #[allow(dead_code)]
 #[cfg_attr(test, derive(arbitrary::Arbitrary))]
