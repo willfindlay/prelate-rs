19414563d137f860e60c032ab18a0878908d1a6d
William Findlay <will@isovalent.com>
2024-02-11 15:51:14 -0500

    convert API to use builders

--- src/lib.rs
+++ src/lib.rs
@@ -14,88 +14,252 @@ mod pagination;
 #[cfg(test)]
 mod testutils;
 
-use anyhow::Result;
-use futures::Stream;
-
-use pagination::{PaginatedRequest, PaginationClient};
-use types::{
-    games::{self, Game, GameKind, GamesOrder, GamesPlayed},
-    profile::{Profile, ProfileId},
-    search::{self, SearchResults},
-};
+use query::{GlobalGamesQuery, ProfileGamesQuery, ProfileQuery, SearchQuery};
+use types::profile::ProfileId;
 
 // Rexports
 pub use chrono;
 pub use futures;
 pub use url;
 
-/// Get profile stats for a player.
+/// Returns a [`ProfileQuery`]. Used to get profile for a player.
+///
+/// # Params
+/// - `profile_id` is aoe4world the ID of the player.
+pub fn profile(profile_id: impl Into<ProfileId>) -> ProfileQuery {
+    ProfileQuery::default().with_profile_id(Some(profile_id.into()))
+}
+
+/// Returns a [`ProfileGamesQuery`]. Used to query the `/profile/{profile_id}/games` endpoint.
 ///
 /// # Params
 /// - `profile_id` is aoe4world the ID of the player whose games should be searched.
-pub async fn profile(profile_id: u64) -> Result<Profile> {
-    reqwest::get(format!("https://aoe4world.com/api/v0/players/{profile_id}"))
-        .await?
-        .json()
-        .await
-        .map_err(anyhow::Error::from)
+pub fn profile_games(profile_id: impl Into<ProfileId>) -> ProfileGamesQuery {
+    ProfileGamesQuery::default().with_profile_id(Some(profile_id.into()))
 }
 
-/// Get games for a player. Games returned as an async stream.
-///
-/// # Params
-/// - `profile_id` is aoe4world the ID of the player whose games should be searched.
-/// - `leaderboard` is an optional leaderboard to be searched against (e.g.
-/// [`Leaderboard::RmTeam`]).
-/// - `opponent_ids` is an optional opponent profile ID to search against.
-/// - `since` is an optional datetime to search after.
-pub async fn games(
-    profile_id: Option<ProfileId>,
-    game_kinds: Option<Vec<GameKind>>,
-    opponent_profile_id: Option<ProfileId>,
-    profile_ids: Option<Vec<ProfileId>>,
-    opponent_profile_ids: Option<Vec<ProfileId>>,
-    since: Option<chrono::DateTime<chrono::Utc>>,
-    order: Option<GamesOrder>,
-) -> Result<impl Stream<Item = Result<Game>>> {
-    let client = PaginationClient::<GamesPlayed, Game>::default();
-    let url = if let Some(profile_id) = profile_id {
-        format!("https://aoe4world.com/api/v0/players/{profile_id}/games").parse()?
-    } else {
-        "https://aoe4world.com/api/v0/games".parse()?
-    };
-    let filter = games::Filter {
-        game_kinds,
-        opponent_profile_id,
-        profile_ids,
-        opponent_profile_ids,
-        since,
-        order,
-    };
-    let url = filter.query_params(url);
-    let pages = client
-        .into_pages_concurrent(PaginatedRequest::new(url))
-        .await?;
-    Ok(pages.items())
+/// Returns a [`GlobalGamesQuery`]. Used to query the `/games` endpoint.
+pub fn games() -> GlobalGamesQuery {
+    GlobalGamesQuery::default()
 }
 
-/// Search for a player. Results returned as an async stream.
+/// Returns a [`SearchQuery`]. Used to query the `/players/search` endpoint.
+///
+/// Note: the query must contain at least 3 characters.
 ///
 /// # Params
-/// - `query` the player name to search for.
-/// - `exact` determines whether the search should exactly match the player name.
-pub async fn search(query: &str, exact: bool) -> Result<impl Stream<Item = Result<Profile>>> {
-    let client = PaginationClient::<SearchResults, Profile>::default();
-    let url = "https://aoe4world.com/api/v0/players/search".parse()?;
-    let filter = search::Filter {
-        query: Some(query.to_owned()),
-        exact: Some(exact),
+/// - `query` is a search query (e.g. a player's username or part of a username).
+pub fn search(query: impl AsRef<str>) -> SearchQuery {
+    SearchQuery::default().with_query(Some(query.as_ref().to_string()))
+}
+
+pub mod query {
+    //! Contains query builders to interact with the aoe4world API.
+    //!
+    //! Using these directly is possible, but it may be more ergonomic to use
+    //! the provided functions at the top-level of the library.
+
+    use anyhow::{bail, Result};
+    use derive_setters::Setters;
+    use futures::Stream;
+    use itertools::join;
+    use url::Url;
+
+    use crate::{
+        pagination::{PaginatedRequest, PaginationClient},
+        types::{
+            games::{Game, GameKind, GamesOrder, GlobalGames, ProfileGames},
+            profile::{Profile, ProfileId},
+            search::SearchResults,
+        },
     };
-    let url = filter.query_params(url);
-    let pages = client
-        .into_pages_concurrent(PaginatedRequest::new(url))
-        .await?;
-    Ok(pages.items())
+
+    /// Constructs a query for the `/players/{profile_id}/games` endpoint.
+    #[derive(Setters, Default)]
+    #[setters(prefix = "with_")]
+    pub struct ProfileGamesQuery {
+        /// [`ProfileId`] to query.
+        profile_id: Option<ProfileId>,
+        /// Filter by game kind category.
+        ///
+        /// NOTE: this is named `leaderboard` but uses the [`GameKind`] enum.
+        leaderboard: Option<Vec<GameKind>>,
+        /// Filter over an opponent's profile ID.
+        opponent_profile_id: Option<ProfileId>,
+        /// Filter over a list of opponent profile IDs.
+        opponent_profile_ids: Option<Vec<ProfileId>>,
+        /// Filter by time played since a specific date.
+        since: Option<chrono::DateTime<chrono::Utc>>,
+    }
+
+    impl ProfileGamesQuery {
+        /// Get the games for this profile.
+        pub async fn get(self) -> Result<impl Stream<Item = Result<Game>>> {
+            if self.profile_id.is_none() {
+                bail!("missing profile_id")
+            }
+
+            let client = PaginationClient::<ProfileGames, Game>::default();
+            let url = format!(
+                "https://aoe4world.com/api/v0/players/{}/games",
+                self.profile_id.unwrap()
+            )
+            .parse()?;
+            let url = self.query_params(url);
+
+            let pages = client
+                .into_pages_concurrent(PaginatedRequest::new(url))
+                .await?;
+            Ok(pages.items())
+        }
+
+        fn query_params(&self, mut url: Url) -> Url {
+            if let Some(ref leaderboard) = self.leaderboard {
+                url.query_pairs_mut()
+                    .append_pair("leaderboard", join(leaderboard, ",").as_str());
+            }
+            if let Some(ref id) = self.opponent_profile_id {
+                url.query_pairs_mut()
+                    .append_pair("opponent_profile_id", id.to_string().as_str());
+            }
+            if let Some(ref ids) = self.opponent_profile_ids {
+                url.query_pairs_mut()
+                    .append_pair("opponent_profile_ids", join(ids, ",").as_str());
+            }
+            if let Some(ref since) = self.since {
+                url.query_pairs_mut()
+                    .append_pair("since", since.to_rfc3339().as_str());
+            }
+            url
+        }
+    }
+
+    /// Constructs a query for the `/games` endpoint.
+    #[derive(Setters, Default)]
+    #[setters(prefix = "with_")]
+    pub struct GlobalGamesQuery {
+        /// Filter by game kind category.
+        ///
+        /// NOTE: this is named `leaderboard` but uses the [`GameKind`] enum.
+        pub leaderboard: Option<Vec<GameKind>>,
+        /// Filter over an opponent's profile ID.
+        pub opponent_profile_id: Option<ProfileId>,
+        /// Filter over a list of profile IDs.
+        pub profile_ids: Option<Vec<ProfileId>>,
+        /// Filter by time played since a specific date.
+        pub since: Option<chrono::DateTime<chrono::Utc>>,
+        /// Filter by time played since a specific date.
+        pub order: Option<GamesOrder>,
+    }
+
+    impl GlobalGamesQuery {
+        /// Get the games.
+        pub async fn get(self) -> Result<impl Stream<Item = Result<Game>>> {
+            let client = PaginationClient::<GlobalGames, Game>::default();
+
+            let url = "https://aoe4world.com/api/v0/games".parse()?;
+            let url = self.query_params(url);
+
+            let pages = client
+                .into_pages_concurrent(PaginatedRequest::new(url))
+                .await?;
+            Ok(pages.items())
+        }
+
+        fn query_params(&self, mut url: Url) -> Url {
+            if let Some(ref leaderboard) = self.leaderboard {
+                url.query_pairs_mut()
+                    .append_pair("leaderboard", join(leaderboard, ",").as_str());
+            }
+            if let Some(id) = self.opponent_profile_id {
+                url.query_pairs_mut()
+                    .append_pair("opponent_profile_id", id.to_string().as_str());
+            }
+            if let Some(ref ids) = self.profile_ids {
+                url.query_pairs_mut()
+                    .append_pair("profile_ids", join(ids, ",").as_str());
+            }
+            if let Some(ref since) = self.since {
+                url.query_pairs_mut()
+                    .append_pair("since", since.to_rfc3339().as_str());
+            }
+            if let Some(ref order) = self.order {
+                url.query_pairs_mut()
+                    .append_pair("order", serde_json::to_string(order).unwrap().as_str());
+            }
+            url
+        }
+    }
+
+    /// Constructs a query for the `/players/{profile_id}` endpoint.
+    #[derive(Setters, Default)]
+    #[setters(prefix = "with_")]
+    pub struct ProfileQuery {
+        /// [`ProfileId`] to query.
+        profile_id: Option<ProfileId>,
+    }
+
+    impl ProfileQuery {
+        /// Get the profile.
+        pub async fn get(self) -> Result<Profile> {
+            if self.profile_id.is_none() {
+                bail!("missing profile_id")
+            }
+
+            reqwest::get(format!(
+                "https://aoe4world.com/api/v0/players/{}",
+                self.profile_id.unwrap()
+            ))
+            .await?
+            .json()
+            .await
+            .map_err(anyhow::Error::from)
+        }
+    }
+
+    /// Constructs a query for the `/players/search` endpoint.
+    #[derive(Setters, Default)]
+    #[setters(prefix = "with_")]
+    pub struct SearchQuery {
+        /// Search query.
+        pub query: Option<String>,
+        /// Should the results exactly match the query.
+        pub exact: Option<bool>,
+    }
+
+    impl SearchQuery {
+        /// Get the search results.
+        pub async fn get(self) -> Result<impl Stream<Item = Result<Profile>>> {
+            if self.query.is_none() {
+                bail!("missing search query");
+            }
+            if self.query.as_slice().len() < 3 {
+                bail!("search query must contain at least 3 characters");
+            }
+
+            let client = PaginationClient::<SearchResults, Profile>::default();
+
+            let url = "https://aoe4world.com/api/v0/players/search".parse()?;
+            let url = self.query_params(url);
+
+            let pages = client
+                .into_pages_concurrent(PaginatedRequest::new(url))
+                .await?;
+            Ok(pages.items())
+        }
+
+        fn query_params(&self, mut url: Url) -> Url {
+            if let Some(query) = &self.query {
+                url.query_pairs_mut()
+                    .append_pair("query", query.to_string().as_str());
+            }
+            if let Some(exact) = self.exact {
+                url.query_pairs_mut()
+                    .append_pair("exact", exact.to_string().as_str());
+            }
+            url
+        }
+    }
 }
 
 #[cfg(test)]
@@ -113,10 +277,12 @@ mod tests {
     #[tokio::test]
     async fn profile_api_smoke() {
         profile(ONLY_CAMS_ID)
+            .get()
             .await
             .expect("API call should succeed");
 
         profile(HOUSEDHORSE_ID)
+            .get()
             .await
             .expect("API call should succeed");
     }
@@ -124,36 +290,22 @@ mod tests {
     #[cfg_attr(not(feature = "test-api"), ignore)]
     #[tokio::test(flavor = "multi_thread")]
     async fn player_games_api_smoke() {
-        let g: Vec<_> = games(
-            Some(ONLY_CAMS_ID.into()),
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-        )
-        .await
-        .expect("API call should succeed")
-        .collect()
-        .await;
+        let g: Vec<_> = profile_games(ONLY_CAMS_ID)
+            .get()
+            .await
+            .expect("API call should succeed")
+            .collect()
+            .await;
         for (i, game) in g.iter().enumerate() {
             assert!(game.is_ok(), "game {i} not ok: {game:?}")
         }
 
-        let g: Vec<_> = games(
-            Some(HOUSEDHORSE_ID.into()),
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-        )
-        .await
-        .expect("API call should succeed")
-        .collect()
-        .await;
+        let g: Vec<_> = profile_games(HOUSEDHORSE_ID)
+            .get()
+            .await
+            .expect("API call should succeed")
+            .collect()
+            .await;
         for (i, game) in g.iter().enumerate() {
             assert!(game.is_ok(), "game {i} not ok: {game:?}")
         }
@@ -162,7 +314,8 @@ mod tests {
     #[cfg_attr(not(feature = "test-api"), ignore)]
     #[tokio::test(flavor = "multi_thread")]
     async fn global_games_api_smoke() {
-        let g: Vec<_> = games(None, None, None, None, None, None, None)
+        let g: Vec<_> = games()
+            .get()
             .await
             .expect("API call should succeed")
             .take(100)
@@ -176,7 +329,9 @@ mod tests {
     #[cfg_attr(not(feature = "test-api"), ignore)]
     #[tokio::test(flavor = "multi_thread")]
     async fn search_api_smoke() {
-        let profiles: Vec<_> = search(ONLY_CAMS_NAME, true)
+        let profiles: Vec<_> = search(ONLY_CAMS_NAME)
+            .with_exact(Some(true))
+            .get()
             .await
             .expect("API call should succeed")
             .collect()
@@ -185,7 +340,9 @@ mod tests {
             assert!(profile.is_ok(), "profile {i} not ok: {profile:?}")
         }
 
-        let profiles: Vec<_> = search(DEBILS_NAME, false)
+        let profiles: Vec<_> = search(DEBILS_NAME)
+            .with_exact(Some(false))
+            .get()
             .await
             .expect("API call should succeed")
             .collect()
